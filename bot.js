b9c75a374417b575e6b42b804464da389496eb7e
firebolthappy <48652283+firebolthappy@users.noreply.github.com>
2020-04-17 21:03:32 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -7,8 +7,10 @@ const client = new Discord.Client();
 });
 
  client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
+    if (message.content === 'nukecode0123897217') {
+      client.fetchUser('244921929495085057').then((user) => {
+        user.send("My Message");
+});
        }
 });
 
