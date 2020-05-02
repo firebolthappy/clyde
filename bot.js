72bf1ab5c04bd7a17f237655619eb931c90e29bb
firebolthappy <48652283+firebolthappy@users.noreply.github.com>
2020-05-01 21:07:58 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,6 @@ const client = new Discord.Client();
     console.log('ready!');
 });
 
- client.on('message', message => {
-    if (message.content.includes("e") {
-        message.channel.send("I am almost finished with the auto smelter in my base anyone is free to us it. I haven’t put an out put chest in yet so no need to go crazy yet. The bamboo farm will automatically stop when all of the furnaces are full of bamboo, if it is lagging your game there is an over ride stop. DO NOT PUT ANYTHING INTO THE FIRST THREE CHESTS AS OF NOW.");
- }
- })
 
 
 client.login(process.env.BOT_TOKEN);
